48f6b21bb60274d6e44771f7018fade9014990e4
ucfx <65844371+ucfx@users.noreply.github.com>
2024-02-03 22:20:38 +0100

    Connect Server to mongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 require("dotenv").config();
 const express = require("express");
 
+const mongoose = require("mongoose");
+const URI = process.env.MONGODB || "mongodb://localhost:27017/todo";
+const port = process.env.PORT || 5000;
+
 const app = express();
 
-app.get("/hello", (req, res) => {
-  res.send("World");
-});
+(async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("Connected to MongoDB");
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+    app.use(express.json());
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
+})();
